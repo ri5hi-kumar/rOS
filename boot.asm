a858807ea3388ee90c85679db5806cd9ce65054a
Rishi Kumar <rishikumar10dec@gmail.com>
2023-07-13 07:17:03 +0530

    read from disk

--- boot.asm
+++ boot.asm
@@ -19,7 +19,22 @@ step2:
 	mov sp, 0x7c00
 	sti
 	
-	mov si, message
+	mov ah, 2
+	mov al, 1
+	mov ch, 0
+	mov cl, 2
+	mov dh, 0
+	mov bx, buffer
+	int 0x13
+	jc error
+
+	mov si, buffer
+	call print
+
+	jmp $
+
+error:
+	mov si, error_message
 	call print
 	jmp $
 
@@ -39,7 +54,9 @@ print_char:
     	int 0x10
 	ret
 
-message: db 'Hello World!', 0
+error_message: db 'Failed to load sector', 0
 
 times 510-($ - $$) db 0
 dw 0xAA55
+
+buffer:
